3c37322226f1014f6096b43e9931ea95a19434e3
josharsh <harsh.joshi.pth@gmail.com>
2021-06-12 20:40:52 +0530

    Minor changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import Home from './components/Home';
+import Docs from "./components/Docs/docs.js"
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 ReactDOM.render(
@@ -11,6 +12,7 @@ ReactDOM.render(
    <Router>
     <Route exact path="/" component={Home} />
     <Route exact path="/play" component={App} />
+    <Route exact path="/docs" component={Docs} />
   </Router>
   </React.StrictMode>,
   document.getElementById('root')
