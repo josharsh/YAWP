97565566d48fedb176a1150ce43ca7d8c003cd01
Sidhaant Thakker <sidhaantthakker@gmail.com>
2021-09-27 16:13:29 +0530

    fix grammatical errors, make code more readable

--- src/components/Docs/docs.js
+++ src/components/Docs/docs.js
@@ -10,8 +10,8 @@ function Docs(){
             <nav id="navbar">
                 <header>YAWP Documentation</header>
                 <ul>
-                    <li><a href="#Try_YAWP" class="nav-link">Try YAWP</a></li>
-                    <li><a href="#Online_Playgrounds" class="nav-link">What are PlayGrounds</a></li>
+                    <li><a href="#Try_YAWP" class="nav-link">Try YAWP!</a></li>
+                    <li><a href="#Online_Playgrounds" class="nav-link">What are Playgrounds?</a></li>
                     <li><a href="#Getting Started" class="nav-link">Getting Started</a></li>
                     <li><a href="#Using Play" class="nav-link">Using Play</a></li>
                     <li><a href="#Contribution" class="nav-link">Contributing Guidelines</a></li>
@@ -23,21 +23,23 @@ function Docs(){
             <main id="main-doc" class="content-docs">
                 <section class="main-section" id="Try_YAWP">
                     <header>
-                        <h3>Try YAWP</h3>
+                        <h3>Try YAWP!</h3>
                     </header>
                     <p>
-                        YAWP is not yet another web playground. At its heart it lets your write code and see results on the fly.
-                        <b>You can use YAWP to write code, see results as you build and take snippets as you go.</b> Whether
-                        you want to get a taste of web, or interactively teach web lessons with options to save what you teach, YAWP docs will help you get started and fix problems as you face them.
+                        YAWP is not just yet another web playground. At its heart it lets your write code and see results on the fly.
+                        <b>You can use YAWP to write code, see the results as you build, and take snippets of it as you go.</b> Whether
+                        you want to get a taste of web, or interactively teach web lessons with options to save what you teach, 
+                        YAWP docs will help you get started and fix problems as you face them.
                     </p>
                 </section>
                 <section class="main-section" id="Online_Playgrounds">
                     <header>
-                        <h3>What is a Playground </h3>
+                        <h3>What is a Playground?</h3>
                     </header>
                     <code>
-                    A web playground does not mind you playing around as you wish. From useful and liberating online code editors to amazing production grade environments, playgrounds on internet help you learn, teach, play, test and more.
-                    
+                    A web playground does not mind you playing around as you wish. 
+                    From useful and liberating online code editors to amazing production grade environments, 
+                    playgrounds on internet help you learn, teach, play, test and more.
                     </code>
                 </section>
                 <section class="main-section" id="Getting Started">
@@ -45,7 +47,8 @@ function Docs(){
                         <h3>Getting Started</h3>
                     </header>
                     <p>
-                        Go to <a href="https://yawp.vercel.app/play ">https://yawp.vercel.app/play </a> and start by typing HTML, CSS and JS on the respective editors.
+                        Go to <a href="https://yawp.vercel.app/play ">https://yawp.vercel.app/play </a> 
+                        and start by typing HTML, CSS and JS on the respective editors.
                     </p>
                 </section>
                 <section class="main-section" id="Using Play">
@@ -53,11 +56,13 @@ function Docs(){
                         <h3>Using Play</h3>
                     </header>
                     <p>
-                        Once you are in YAWP, you can quickly start by writing custom HTML, CSS and Javascript. The results will appear on the right as you type.
+                        Once you are in YAWP, you can quickly start by writing custom HTML, CSS and Javascript. 
+                        The results will appear on the right as you type.
                         If you are using YAWP for interactive lessons, it is recommended to use the floating action button for utilities. 
                     <ul>
                         <li>
-                        You can make a snippet of the code on screen using the screenshot tool. The screenshot tool can be accessed using the bottom right floating action button.
+                        You can make a snippet of the code on screen using the screenshot tool. 
+                        The screenshot tool can be accessed using the bottom right floating action button.
                         </li>
                         <li>
                         You can minimize the editor panes using the top right buttons on the code editors.
@@ -66,7 +71,8 @@ function Docs(){
                         To clear content, use the Eraser tool from the actions menu.
                         </li>
                         <li>
-                        YAWL utilizes browser local storage to store your work. Next time you come to use YAWP, you can start where you left.
+                        YAWL utilizes browser local storage to store your work. 
+                        Next time you come to use YAWP, you can start where you left.
                         </li>
                     </ul>
                     </p>
